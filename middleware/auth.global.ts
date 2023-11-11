d523427900b639847da356f1b144ee7a4499c98a
hadidbilla <hadidbilla449@gmail.com>
2023-11-11 21:34:02 +0600

    add gard

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,7 +1,7 @@
 export default defineNuxtRouteMiddleware((to, from) => {
   const token = useCookie("token")
   // @ts-ignore
-  // if (!token.value && (to.name !== "login" && to.name !== "signup")) {
-  //   return navigateTo('/login');
-  // }
+  if (!token.value && (to.name !== "login" && to.name !== "signup")) {
+    return navigateTo('/login');
+  }
 })
